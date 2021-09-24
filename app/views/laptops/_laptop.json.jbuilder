15ffed3ee9c4f8092c3b6036142f54923250a4d2
muhammadbilalasgharali <bilal.asghar@devsinc.com>
2021-09-24 19:17:21 +0500

    laptop push

---
+++ app/views/laptops/_laptop.json.jbuilder
@@ -0,0 +1,2 @@
+json.extract! laptop, :id, :name, :created_at, :updated_at
+json.url laptop_url(laptop, format: :json)
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_09_24_140156) do
+ActiveRecord::Schema.define(version: 2021_09_24_141443) do
+
+  create_table "laptops", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "unis", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "name"
